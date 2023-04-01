99f5bebee1d358c4d13d8f55ad4d73aaabe336a8
karencfisher <karencfisher@hotmail.com>
2023-04-01 13:57:12 -0600

    I think we've got a transformer encoder

--- encoder.py
+++ encoder.py
@@ -5,10 +5,10 @@ from positional import PositionalEmbeddings
 from feed_forward import FeedForward
 
 
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, config, name="Encoder"):
-        super(Encoder, self).__init__(name=name)
-        self.layer_norm1 = tf.keras.layers.LayerNormalization()
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, config, name='EncoderLayer'):
+        super(EncoderLayer, self).__init__(name=name)
+        self.layer_norm1 = tf.keras.layers.LayerNormalization(input_shape=config['input_size'])
         self.layer_norm2 = tf.keras.layers.LayerNormalization()
         self.attention = MultiAttentionHead(config)
         self.ff = FeedForward(config)
@@ -18,4 +18,17 @@ class Encoder(tf.keras.layers.Layer):
         x = x + self.attention(x)
         x = self.layer_norm2(x)
         x = self.ff(x)
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, config, name='Encoder'):
+        super(Encoder, self).__init__(name=name)
+        self.embeddings = PositionalEmbeddings(config)
+        self.layers = [EncoderLayer(config) for _ in range(config['num_hidden_layers'])]
+
+    def call(self, x):
+        x = self.embeddings(x)
+        for layer in self.layers:
+            x = layer(x)
         return x
