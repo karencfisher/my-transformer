37b0d8c2c3827e58425cab6b1c3a6bcc9333e33b
karencfisher <karencfisher@hotmail.com>
2023-04-22 12:48:20 -0600

    saving encoder model test on sentiment

--- transformer/encoder.py
+++ transformer/encoder.py
@@ -8,10 +8,11 @@ from transformer.feed_forward import FeedForward
 class EncoderLayer(tf.keras.layers.Layer):
     def __init__(self, config, name='EncoderLayer'):
         super(EncoderLayer, self).__init__(name=name)
-        self.layer_norm1 = tf.keras.layers.LayerNormalization(input_shape=config['input_size'])
+        self.config = config
+        self.layer_norm1 = tf.keras.layers.LayerNormalization(input_shape=self.config['input_size'])
         self.layer_norm2 = tf.keras.layers.LayerNormalization()
-        self.attention = MultiAttentionHead(config)
-        self.ff = FeedForward(config)
+        self.attention = MultiAttentionHead(self.config)
+        self.ff = FeedForward(self.config)
 
     def call(self, embeds):
         x = self.layer_norm1(embeds)
@@ -22,13 +23,19 @@ class EncoderLayer(tf.keras.layers.Layer):
 
 
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self, config, name='Encoder'):
-        super(Encoder, self).__init__(name=name)
-        self.embeddings = PositionalEmbeddings(config)
-        self.layers = [EncoderLayer(config) for _ in range(config['num_hidden_layers'])]
+    def __init__(self, config, name='Encoder', **kwargs):
+        super(Encoder, self).__init__(name=name, **kwargs)
+        self.config = config
+        self.embeddings = PositionalEmbeddings(self.config)
+        self.layers = [EncoderLayer(self.config) for _ in range(config['num_hidden_layers'])]
 
     def call(self, x):
         x = self.embeddings(x)
         for layer in self.layers:
             x = layer(x)
         return x
+    
+    def get_config(self):
+        config = super(Encoder, self).get_config()
+        config.update({"config": self.config})
+        return config
